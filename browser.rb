1cabb29fc37d866805794e041ab37d423ca74da4
Keil Hunsaker <keilhun@gmail.com>
2016-02-23 10:41:15 -0500

    Completed Handling GET requests

--- browser.rb
+++ browser.rb
@@ -1,9 +1,10 @@
 require 'socket'
- 
+require 'pp'
+
 host = 'localhost'     # The web server
 #port = 80                           # Default HTTP port
 port = 2000
-path = "/index.html"                 # The file we want 
+path = "/index.htm"                 # The file we want 
 
 # This is the HTTP request we send to fetch a file
 request = "GET #{path} HTTP/1.0\r\n\r\n"
@@ -13,6 +14,18 @@ socket = TCPSocket.open(host,port)  # Connect to server
 socket.print(request)               # Send request
 response = socket.read              # Read complete response
 # Split response at first blank line into headers and body
-headers,body = response.split("\r\n\r\n", 2)
-print headers
-print body                          # And display it
+headers,body = response.split("\n\n", 2)
+header_lines = headers.split("\n")
+# match[1]=transport type, match[2]=result code, match[3]=Result Message
+match = /(\w+\/\d.\d)\s(\d+)\s(.+)/.match(header_lines[0])
+
+case match[2]
+  when "200"
+    puts body
+  else
+    puts match[3]
+end
+
+#pp header_lines
+#print headers
+#print body                          # And display it
